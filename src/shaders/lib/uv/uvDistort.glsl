183442ecfd7468c2acf8c3b88ee0a1c375259450
otanodesignco <otanodesign@gmail.com>
2025-03-19 02:08:33 -0500

    fixed uvDistort function

--- src/shaders/lib/uv/uvDistort.glsl
+++ src/shaders/lib/uv/uvDistort.glsl
@@ -5,7 +5,7 @@ vec2 uvDistort( vec2 uv, sampler2D noise, vec2 pan, float time, float intensity 
     vec2 uvPanned = uv + timed;
 
     float uvNoise = texture( noise, uvPanned ).r;
-    uvNoise *= intensity;
+    uvNoise = ( uvNoise - 0.5 ) * intensity;
 
     return uv1 + uvNoise;
 
